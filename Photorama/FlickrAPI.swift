1d1ac13053f453db7ced2e324c1af7b4e9180d0d
Mimi Chenyao <mimichenyao@gmail.com>
2017-09-14 18:15:42 -0400

    Added FlickrAPI

---
+++ Photorama/FlickrAPI.swift
@@ -0,0 +1,13 @@
+//
+//  FlickrAPI.swift
+//  Photorama
+//
+//  Created by Mimi Chenyao on 9/14/17.
+//  Copyright © 2017 Mimi Chenyao. All rights reserved.
+//
+
+import Foundation
+
+struct FlickrAPI {
+    
+}
