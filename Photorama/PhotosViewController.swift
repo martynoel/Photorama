2e1bd99a659780460bbf9edc29dd76328d5c5236
Mimi Chenyao <mimichenyao@gmail.com>
2017-09-14 17:45:36 -0400

    Added first views

---
+++ Photorama/PhotosViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Mimi Chenyao on 9/14/17.
+//  Copyright © 2017 Mimi Chenyao. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+}
